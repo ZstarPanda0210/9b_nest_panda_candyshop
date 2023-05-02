2cb8b512d117dcb3c78db687c3247fad63d3dd47
Pnda <106011917+ZstarPanda0210@users.noreply.github.com>
2023-05-02 00:50:09 +0000

    5.2

--- product2.js
+++ product2.js
@@ -21,7 +21,7 @@ const products = [
       description: "Candy",
       type: "hard,sour",
       image:
-        "https://www.nicepng.com/png/detail/353-3539716_siriusxm-pop-rocks.png ",
+        "https://candy-shop.pl/userdata/public/gfx/4156/pop-rocks-strawberry.png",
     },
     {
       name: "M&m",
@@ -50,14 +50,100 @@ const products = [
     {
       name: "Twixx",
       price: 2100,
+      description: "chocolate",
+      type: "chocolate",
+      image:
+        "https://www.pngplay.com/wp-content/uploads/15/Twix-Cookie-Bars-Transparent-File.png",
+    },
+    {
+      name: "Nerds",
+      price: 1900,
+      description: "rock type of candy",
+      type: "rock",
+      image:
+        "https://assets.stickpng.com/thumbs/6016ee353e22aa0004b22d49.png",
+    },
+    {
+      name: "Star Burst",
+      price: 2000,
+      description: "Candy",
+      type: "sugary",
+      image:
+        "https://assets.stickpng.com/images/601571b9a038420004234a85.png",
+    },
+    {
+      name: "Sour Patch",
+      price: 2100,
+      description: "sour type of candy",
+      type: "",
+      image:
+        "https://assets.stickpng.com/images/601592dc5cc23e000407ba22.png",
+    },
+    {
+      name: "Juicy Drop",
+      price: 2100,
+      description: "liquid,sour type of candy",
+      type: "chocolate",
+      image:
+        "https://cdn.shopify.com/s/files/1/1341/1521/products/Gummies_1_600x.png?v=1625673439",
+    },
+    {
+      name: "Jolly Ranchers",
+      price: 2100,
+      description: "Candy",
+      type: "chocolate",
+      image:
+        "https://assets.stickpng.com/images/60158fa65cc23e000407ba1a.png",
+    },
+    {
+      name: "Hershey's",
+      price: 2100,
+      description: "milk chocolate",
+      type: "milk chocolate",
+      image:
+        "https://www.nicepng.com/png/full/155-1553367_hershey-s-milk-chocolate-hershey-chocolate-bar-transparent.png",
+    },
+    {
+      name: "Sour punch",
+      price: 2100,
+      description: "Candy",
+      type: "chocolate",
+      image:
+        "https://www.nicepng.com/png/full/117-1170277_strawberry-sour-punch-straws-candy-sour-punch-straws.png",
+    },
+    {
+      name: "Chupa Chups",
+      price: 2100,
+      description: "Lollipop",
+      type: "chocolate",
+      image:
+        "https://www.pngmart.com/files/10/Chupa-Chups-Lollipop-PNG-Image.png",
+    },
+    {
+      name: "Twixx",
+      price: 2100,
+      description: "Candy",
+      type: "chocolate",
+      image:
+        "https://www.pngplay.com/wp-content/uploads/15/Twix-Cookie-Bars-Transparent-File.png",
+    },
+    {
+      name: "Twixx",
+      price: 2100,
+      description: "Candy",
+      type: "chocolate",
+      image:
+        "https://www.pngplay.com/wp-content/uploads/15/Twix-Cookie-Bars-Transparent-File.png",
+    },
+    {
+      name: "Twixx",
+      price: 2100,
       description: "Candy",
       type: "chocolate",
       image:
         "https://www.pngplay.com/wp-content/uploads/15/Twix-Cookie-Bars-Transparent-File.png",
     },
   ];
-  
-  
   function PrintProducts() {
     const productsContainer = document.getElementById("products");
     let newDiv = "";
